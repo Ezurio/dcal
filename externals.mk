45988f38c7e93a53bd60df48c2c4f7b9066f3a00
Steve deRosier <steve.derosier@lairdtech.com>
2016-07-08 15:56:03 -0700

    update external project makefile so that it builds properly with new Makefile
    The newer Makefile changes expectations where things go, this fixes it so that
    basic builds work and the instructions in the README are still accurate.
    
    Also upgrades flatcc to 0.3.3

--- externals.mk
+++ externals.mk
@@ -38,34 +38,34 @@ CXX ?= g++
 all : libssh flatcc
 
 
-lib:
-	mkdir -p lib
+lib.local:
+	mkdir -p lib.local
 
 #
 # libssh
 #
-lib/libssh: lib
-	cd lib && git clone git://git.libssh.org/projects/libssh.git
+lib.local/libssh: lib.local
+	cd lib.local && git clone git://git.libssh.org/projects/libssh.git
 
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Linux)
-LIBSSH_TARGET := lib/libssh/build/src/libssh.so.4.4.0
+LIBSSH_TARGET := lib.local/libssh/build/src/libssh.so.4.4.0
 LIBSSH_INSTALL := api/lib/libssh.so.4.4.0
 endif
 ifeq ($(UNAME_S),Darwin)
-LIBSSH_TARGET := lib/libssh/build/src/libssh.4.4.0.dylib
+LIBSSH_TARGET := lib.local/libssh/build/src/libssh.4.4.0.dylib
 LIBSSH_INSTALL := api/lib/libssh.4.4.0.dylib
 endif
 
 
-$(LIBSSH_TARGET): lib lib/libssh
-	cd lib/libssh && git checkout 4d43fbfb50710055352c4fda812b6dc98143d336
-	mkdir -p lib/libssh/build
-	cd lib/libssh/build && cmake -DCMAKE_INSTALL_PREFIX=$(BASE_DIR)/api ..
-	cd lib/libssh/build && make
+$(LIBSSH_TARGET): lib.local lib.local/libssh
+	cd lib.local/libssh && git checkout 4d43fbfb50710055352c4fda812b6dc98143d336
+	mkdir -p lib.local/libssh/build
+	cd lib.local/libssh/build && cmake -DCMAKE_INSTALL_PREFIX=$(BASE_DIR)/api ..
+	cd lib.local/libssh/build && make
 
 $(LIBSSH_INSTALL): $(LIBSSH_TARGET)
-	cd lib/libssh/build && make install
+	cd lib.local/libssh/build && make install
 
 clean_libssh:
 	-rm -rf api/lib/
@@ -77,26 +77,26 @@ libssh: $(LIBSSH_TARGET) $(LIBSSH_INSTALL)
 #
 # flatcc
 #
-lib/flatcc : lib
-	cd lib && git clone git@github.com:dvidelabs/flatcc.git
-	cd lib/flatcc && git checkout v0.2.0
-	cd lib/flatcc && patch -p0 < ../../patches/flatcc001_ninja-to-make.patch
+lib.local/flatcc : lib.local
+	cd lib.local && git clone git@github.com:dvidelabs/flatcc.git
+	cd lib.local/flatcc && git checkout v0.3.3
+	cp lib.local/flatcc/scripts/build.cfg.make lib.local/flatcc/scripts/build.cfg
 
-lib/flatcc/lib/libflatcc.a : lib/flatcc
-	cd lib/flatcc && ./scripts/build.sh
+lib.local/flatcc/lib/libflatcc.a : lib.local/flatcc
+	cd lib.local/flatcc && ./scripts/build.sh
 
 LIBFLATCC_INSTALL = api/libflatcc.a
-$(LIBFLATCC_INSTALL) : lib/flatcc/lib/libflatcc.a
-	cp lib/flatcc/lib/*.a api/.
+$(LIBFLATCC_INSTALL) : lib.local/flatcc/lib/libflatcc.a
+	cp lib.local/flatcc/lib/*.a api/.
 
 .PHONY: flatcc
-flatcc: lib/flatcc/lib/libflatcc.a $(LIBFLATCC_INSTALL)
+flatcc: lib.local/flatcc/lib/libflatcc.a $(LIBFLATCC_INSTALL)
 
 .PHONY: clean_flatcc
 clean_flatcc:
 	-rm -f api/libflatcc*.a
 
-export FLATCC := lib/flatcc/bin/flatcc
+export FLATCC := lib.local/flatcc/bin/flatcc
 
 #
 # Utility
@@ -104,7 +104,7 @@ export FLATCC := lib/flatcc/bin/flatcc
 
 .PHONY: clean
 clean : clean_libssh clean_flatcc
-	-rm -rf lib
+	-rm -rf lib.local
 
 .PHONY: cleanall
 cleanall: clean
