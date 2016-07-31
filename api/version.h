8eaa8e49ab436c9f3cd85c8a77581dbfab0d77b3
Eric Bentley <eric.bentley@lairdtech.com>
2016-07-31 00:47:23 -0400

    set up version #DEFINEs for DCAL Component and DCAL API

--- api/version.h
+++ api/version.h
@@ -1,4 +1,17 @@
-// version component: 91 = 0x5b
-// SDK version 3.x.y.z
+//the component value should not change
+#define DCAL_LAIRD_COMPONENT    91
 
-#define DCAL_API_VERSION 0x5b030101
+//the next three values define the API version between DCAL and DCAS
+#define LAIRD_SDK_MSB       3
+#define LAIRD_DCAL_MAJOR    1
+#define LAIRD_DCAL_MINOR    1
+
+// the following #DEFINES should not be modified by hand
+// -----------------------------------------------------
+#define STR_VALUE(arg) #arg
+#define ZVER(name) STR_VALUE(name)
+
+#define DCAL_VERSION_STR ZVER(LAIRD_SDK_MSB) "." ZVER(LAIRD_DCAL_MAJOR) "." ZVER(LAIRD_DCAL_MINOR)
+#define DCAL_VERSION ((LAIRD_SDK_MSB << 16) | (LAIRD_DCAL_MAJOR << 8) | LAIRD_DCAL_MINOR)
+#define DCAL_COMPONENT_VERSION ((DCAL_LAIRD_COMPONENT << 24) | (LAIRD_SDK_MSB << 16) | (LAIRD_DCAL_MAJOR << 8) | LAIRD_DCAL_MINOR)
+// -----------------------------------------------------
