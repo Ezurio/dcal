038e520f808af8dd2c167fe68bacf803b1c75a7e
Joe Conley <joe.conley@lairdtech.com>
2018-03-28 09:28:00 -0400

    laird: dcal: update libssh version to 0.7.3
    bug 12752

--- externals.mk
+++ externals.mk
@@ -49,17 +49,17 @@ lib.local/libssh: lib.local
 
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Linux)
-LIBSSH_TARGET := lib.local/libssh/build/src/libssh.so.4.4.0
-LIBSSH_INSTALL := api/lib/libssh.so.4.4.0
+LIBSSH_TARGET := lib.local/libssh/build/src/libssh.so.4.4.1
+LIBSSH_INSTALL := api/lib/libssh.so.4.4.1
 endif
 ifeq ($(UNAME_S),Darwin)
-LIBSSH_TARGET := lib.local/libssh/build/src/libssh.4.4.0.dylib
-LIBSSH_INSTALL := api/lib/libssh.4.4.0.dylib
+LIBSSH_TARGET := lib.local/libssh/build/src/libssh.4.4.1.dylib
+LIBSSH_INSTALL := api/lib/libssh.4.4.1.dylib
 endif
 
 
 $(LIBSSH_TARGET): lib.local lib.local/libssh
-	cd lib.local/libssh && git checkout 4d43fbfb50710055352c4fda812b6dc98143d336
+	cd lib.local/libssh && git checkout libssh-0.7.3
 	mkdir -p lib.local/libssh/build
 	cd lib.local/libssh/build && cmake -DCMAKE_INSTALL_PREFIX=$(BASE_DIR)/api ..
 	cd lib.local/libssh/build && make
